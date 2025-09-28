9993b928eebeb75d937beaac9b9063e847eb63ca
yogakrishanthi98 <yogakrishanthi@konnect.team>
2025-09-28 11:07:12 +0530

    push new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  // let sangam = 'unused variable here';
+  let sangam = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
