d456dbd4741f54902e41a4d8f6fb5d3b4f61e3a0
yogakrishanthi98 <yogakrishanthi@konnect.team>
2025-09-28 10:51:16 +0530

    changes

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,9 +1,10 @@
-export default [
+// eslint.config.js
+module.exports = [
   {
     files: ['**/*.js'], // only check .js files in server directory
     rules: {
-      semi: 'error', //force semicolons
-      'no-unused-vars': 'warn', //warn if variables are unused
+      semi: 'error', // force semicolons
+      'no-unused-vars': 'warn', // warn if variables are unused
     },
   },
 ];
